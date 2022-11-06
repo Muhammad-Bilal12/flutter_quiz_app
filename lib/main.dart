6c225e9879633ebf7215b6b74d1e636c287a0d0c
muhammad bilal <muhammadbilalmnaeem@gmail.com>
2022-11-06 20:36:55 +0500

    Add QuestionAnswer class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,21 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = <Icon>[];
-  List<String> questions = <String>[
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+// Questuin class Instance
+  Question q1 = Question(
+      q: 'You can lead a cow down stairs but not up stairs.', a: false);
+
+  List<Question> questionList = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: false),
   ];
+
   int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -44,7 +54,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionList[questionNumber].questionText!,
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   fontSize: 25.0,
@@ -69,6 +79,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool checkAnswer = questionList[questionNumber].questionAnswer!;
+
+                if (checkAnswer == true) {
+                  print("The Answer Is right!");
+                } else {
+                  print("Wrong Answer!");
+                }
+
                 setState(() {
                   questionNumber++;
                   scoreKeeper.add(const Icon(
@@ -94,6 +112,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+
+                bool checkAnswer = questionList[questionNumber].questionAnswer!;
+
+                if (checkAnswer == false) {
+                  print("The Answer Is right!");
+                } else {
+                  print("Wrong Answer!");
+                }
+
                 setState(() {
                   questionNumber++;
                   scoreKeeper.add(const Icon(
---
+++ lib/questions.dart
@@ -0,0 +1,11 @@
+class Question {
+  String? questionText;
+  bool? questionAnswer;
+
+// Constructor Function
+
+  Question({String? q, bool? a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
